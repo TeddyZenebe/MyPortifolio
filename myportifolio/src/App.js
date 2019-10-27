a5a2930d594113bd279b59cffe879c54acc61251
teddyzenebe <tfikrie@yahoo.com>
2019-10-27 14:10:42 -0400

    about component created and exported

--- myportifolio/src/App.js
+++ myportifolio/src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import {NavLink, Route} from 'react-router-dom'
-import './App.css';
+import './App.css'
+import About from './components/about.js'
+import projects from './components/projects.js'
+import contact from './components/contact.js'
 
 function App() {
   return (
     <div className="App">
-     <NavLink to='/'>HOME</NavLink>
-     <NavLink to='/'>ABOUT</NavLink>
-     <NavLink to='/'>PROJECT</NavLink>
-     <NavLink to='/'>CONTACT</NavLink>
+      <div className="Nav">
+        <NavLink to='/'>HOME</NavLink>
+        <NavLink to='/about'>ABOUT</NavLink>
+        <NavLink to='/project'>PROJECT</NavLink>
+        <NavLink to='/contact'>CONTACT</NavLink>
+      </div>
+      <div className="Intro">
+       Hello, Welcome to My Portifolio Page. <br />I am Teddy Zenebe, Full Stack Developer<br />
+       For More Information Checkout Above^^
+      </div>
+
      <Route exact path="/" />
-     <Route exact path="/" component='about' />
-     <Route exact path="/" component='projects' />
-     <Route exact path="/" component='contact' />
+     <Route exact path="/about" component={About} />
+     <Route exact path="/project" component='projects' />
+     <Route exact path="/contact" component='contact' />
     </div>
   );
 }
