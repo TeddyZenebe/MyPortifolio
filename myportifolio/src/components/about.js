24892c556dc9c40a757fd05fae41d37f57b2a7a3
teddyzenebe <tfikrie@yahoo.com>
2020-01-11 21:53:28 -0500

    about page update

--- myportifolio/src/components/about.js
+++ myportifolio/src/components/about.js
@@ -5,21 +5,26 @@ function About() {
     return (
         <div className='About'>
           
-          <div className='Abouteddy' > 
-          <h2>Hi, My Name is Tewodros Zenebe </h2>
-          <p>I am a full-stack web developer leaving in Charlotte, NC. I have more than 5 years of work expriance in differen tech related work.
-             I have more than a year expriance in developing user friendely responsive web app using HTML5, CSS3, Javascript, React, Bootstrap, JQurey ... &#xe234;
+          <div className='Abou-teddy' > 
+          <h1>It is my Pleasure to see you here, My Name is Teddy Zenebe </h1>
+          <p>I am a full-stack web application developer leaving in Charlotte, NC. I have a total of 6 years of work 
+              expriance in differen tech related work. Out of 6 years work expriance, 3 of them were in developing user 
+              friendely responsive web applications using HTML5, CSS3, Javascript, React, ArcGis API, Bootstrap, JQurey ... &#xe234;
           </p>
           <p>
-             I also have expriance creating REST full API using Node.js (express). I certified from Microsofte in MSSQL Server database adminstration and
-             developmant.
+             I also have morthan 2 years expriance creating RESTful API using Node.js (express library). I have more than 3 years
+             expriance developing and managing MSSQL Server databases. I certified from Microsofte in MSSQL Server database adminstration 
+             and developmant.
+          </p>
+          <p>Recently I participated in the project that builed wising .NET(core) at back end and Anglar for front end. I have laso some exxposure 
+            o developing application using Pyton.
           </p>
           <p>
               To checkout some of my projects <NavLink to='/project'>Click Here</NavLink>
           </p>
           </div>
           <div>
-              <img src= {teddy} alt='my look' className='Teddy' />
+              <img src= {teddy} alt='my look' className='Teddy-Image' />
           </div>
 
         </div>
