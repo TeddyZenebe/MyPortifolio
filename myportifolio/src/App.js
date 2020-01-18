6edaf3cf30bd0d8708baee2c5707eab63b96db15
Teddy <tfikrie@yahoo.com>
2020-01-18 09:38:49 -0500

    Material UI used for the home page

--- myportifolio/src/App.js
+++ myportifolio/src/App.js
@@ -1,26 +1,185 @@
 import React from 'react';
-import {NavLink, Route} from 'react-router-dom'
-import './App.css'
-import Home from './components/home.js'
-import About from './components/about.js'
-import projects from './components/projects.js'
-import contact from './components/contact.js'
+// import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import HomeWorkIcon from '@material-ui/icons/HomeWork';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InfoIcon from '@material-ui/icons/Info';
+import WorkIcon from '@material-ui/icons/Work';
+import ContactMailIcon from '@material-ui/icons/ContactMail';
+import PictureAsPdfIcon from '@material-ui/icons/PictureAsPdf';
+import SportsSoccerIcon from '@material-ui/icons/SportsSoccer';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Home from './components/home'
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: 200,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - 200px)`,
+      marginLeft: 200,
+      background: 'red'
+    },
+  },
+  appBarWord : {
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: 100,
+      fontFamily: 'Allerta Stencil'
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: 200,
+    background:'#003300',
+    opacity: 0.5,
+    fontSize: '3.5rem',
+    fontFamily: 'Allerta Stencil',
+    color: '#ffffff'
+  },
+  content: {
+    flexGrow: 1,
+    height: 'auto',
+    padding: theme.spacing(3),
+    background:'#bfbfbf',
+  },
+}));
+
+function App(props) {
+  const { container } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div >
+      <div className={classes.toolbar} >Hello</div>
+      <Divider />
+      <List>
+          <ListItem button onClick={() => {
+            navigate(action.path);
+            handleClose();
+          }}>
+            <ListItemIcon> <HomeWorkIcon/> </ListItemIcon>
+            <ListItemText> HOME </ListItemText>
+          </ListItem>
+          <ListItem button >
+            <ListItemIcon> <InfoIcon /> </ListItemIcon>
+            <ListItemText> ABOUT </ListItemText>
+          </ListItem>
+          <ListItem button >
+            <ListItemIcon> <WorkIcon /> </ListItemIcon>
+            <ListItemText> PROJECT </ListItemText>
+          </ListItem> 
+      </List>
+      <Divider />
+      <List>
+          <ListItem button> 
+            <ListItemIcon> <ContactMailIcon/> </ListItemIcon>
+            <ListItemText> CONTACT </ListItemText>
+          </ListItem>
+          <ListItem button> 
+            <ListItemIcon> <PictureAsPdfIcon/> </ListItemIcon>
+            <ListItemText> RESUME </ListItemText>
+          </ListItem>
+          <ListItem button> 
+            <ListItemIcon> <SportsSoccerIcon/> </ListItemIcon>
+            <ListItemText> HOBYY </ListItemText>
+          </ListItem>
+      </List>
+    </div>
+  );
+
   return (
-    <div className="App">
-      <div className="Nav">
-        <NavLink className='Nav-menu' to='/'>HOME</NavLink>
-        <NavLink className='Nav-menu' to='/about'>ABOUT</NavLink>
-        <NavLink className='Nav-menu' to='/project'>PROJECT</NavLink>
-        <NavLink className='Nav-menu' to='/contact'>CONTACT</NavLink>
-      </div>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/project" component={projects} />
-      <Route exact path="/contact" component={contact} />
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h4" Wrap className={classes.appBarWord}>
+            Oportunity did not Knock Untill I Built a Door!
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Home />
+      </main>
     </div>
   );
 }
+
+// App.propTypes = {
+//   /**
+//    * Injected by the documentation to work in an iframe.
+//    * You won't need it on your project.
+//    */
+//   container: PropTypes.instanceOf(typeof Element === 'undefined' ? Object : Element),
+// };
 
 export default App;
