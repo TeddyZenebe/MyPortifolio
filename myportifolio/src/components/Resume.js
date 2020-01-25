3e4044b518c75f8c6506fda0f9c2df3559994ce5
teddyzenebe <tfikrie@yahoo.com>
2020-01-25 15:12:22 -0500

    cv mobile size and deskto size css added

--- myportifolio/src/components/Resume.js
+++ myportifolio/src/components/Resume.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import CV from '../img/CV.jpg';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+ cv: {
+     width:'100%'
+ }
+}));
 
 const Resume = (props)=> {
+    const classes = useStyles();
     return (
         <div>
             <h1>Resume updated Nov/2019</h1>
-            <img src={CV} alt ='TZ_Resume'/>
+            <img src={CV} alt ='TZ_Resume'className={classes.cv} />
         </div>
     )
 }
