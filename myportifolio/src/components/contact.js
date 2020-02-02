1d1068fbbca7674f6bd40506cbce6c7d0df78275
teddyzenebe <tfikrie@yahoo.com>
2020-02-02 15:09:23 -0500

    mobile size css added

--- myportifolio/src/components/contact.js
+++ myportifolio/src/components/contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../App.css'
 import { Formik, Field, Form } from 'formik';
 import * as Yup from 'yup';
 import { MdEmail, MdMessage, MdPerson, MdPhone } from 'react-icons/md';
@@ -18,47 +19,15 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
 
-  contactRaper: {
-    width: '80%',
-    display: 'flex',
-    color: '#003d99',
-    fontSize: '2rem',
-    borderRadius: '20px',
-    marginTop: '5%',
-    marginLeft: '10%',
-    background: '#808080',
-    marginBottom: '10%',
-    padding: '5%'
-  },
-  contactMessage : {
-    width: '48%'
-  },
-  contactInfo : {
-    width: '48%',
-    marginLeft: '10%'
-  },
   inputWraper: {
     padding: '2%'
   },
-  input : {
-    width: '60%',
-    fontSize: '1.5rem',
-    borderRadius: '10px',
-    background: '#e6e6e6',
-    marginLeft: '2%'
-  },
-  inputMessage : {
-    fontSize: '1.5rem',
-    borderRadius: '10px',
-    background: '#e6e6e6',
-    marginLeft: '25%',
-    width: '60%'
-  },
-  butWrapper : {
+  
+  butWrapper: {
     margin: '2%',
     marginLeft: '35%'
   },
-  butSubmit : {
+  butSubmit: {
     background: '#99ff99',
     marginLeft: '10%'
   }
@@ -105,127 +74,133 @@ export default function ContactForm() {
   };
 
   return (
-    <div className={classes.contactRaper}>
-      
-        <div className={classes.contactMessage}>
-          <h2>Contact Me</h2>
-          <Formik
-            validationSchema={validationSchema}
-            initialValues={{
-              name: '',
-              email: '',
-              message: '',
-              'bot-field': '',
-              'form-name': 'contact'
-            }}
-            onSubmit={(values, { resetForm }) => {
-              fetch('/', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                    body: encode({
-                      'form-name': 'contact',
-                      ...values
-                    })
-                  })
-                .then(() => {
-                  resetForm();
-                  handleOpen();
-                })
-                .catch(err => console.error(err));
-            }}
-          >
-            {({ errors, touched, isSubmitting }) => (
-              <Form>
-                <div className={classes.inputWraper}>
-                  <label htmlFor="name">
-                    <MdPerson /> {' '}
-                    Name 
+    <div className={classes.contactRaper} className='contactRaper'>
+
+      <div className='contactMessage'>
+        <h2>Contact Me</h2>
+        <Formik
+          validationSchema={validationSchema}
+          initialValues={{
+            name: '',
+            email: '',
+            message: '',
+            'bot-field': '',
+            'form-name': 'contact'
+          }}
+          onSubmit={(values, { resetForm }) => {
+            fetch('/', {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+              body: encode({
+                'form-name': 'contact',
+                ...values
+              })
+            })
+              .then(() => {
+                resetForm();
+                handleOpen();
+              })
+              .catch(err => console.error(err));
+          }}
+        >
+          {({ errors, touched, isSubmitting }) => (
+            <Form>
+              <div className={classes.inputWraper}>
+                <label htmlFor="name">
+                  <MdPerson /> {' '}
+                  Name
                  </label>{' '}
-                  <Field placeholder="Enter your name" name="name" type="text" className={classes.input}/>
-                  {errors.name && touched.name ? (<div style={{color:'red', marginLeft:'20%', fontSize:'1.5rem'}}><FaExclamation />{errors.name}</div>) : null}
-                </div>
-                <div className={classes.inputWraper}>
-                  <label htmlFor="email">
-                    <MdEmail />
-                    {' '}
-                    Email
+                <Field placeholder="Enter your name" 
+                       name="name" 
+                       type="text" 
+                       className={classes.input} 
+                       className='input'/>
+                {errors.name && touched.name ? (<div style={{ color: 'red', marginLeft: '20%', fontSize: '1.5rem' }}><FaExclamation />{errors.name}</div>) : null}
+              </div>
+              <div className={classes.inputWraper}>
+                <label htmlFor="email">
+                  <MdEmail />
+                  {' '}
+                  Email
               </label> {' '}
-                  <Field
-                    placeholder="Enter your email address"
-                    name="email"
-                    type="email"
-                    className={classes.input}
-                  />
-                  {errors.email && touched.email ? (<div style={{color:'red', marginLeft:'20%', fontSize:'1.5rem'}}><FaExclamation />{errors.email}</div>) : null}
-                </div>
-                <div className={classes.inputWraper}>
-                  <label htmlFor="message">
-                    <MdMessage />
-                    {' '}
-                    Message
+                <Field
+                  placeholder="Enter your email address"
+                  name="email"
+                  type="email"
+                  className={classes.input}
+                  className='input'
+                />
+                {errors.email && touched.email ? (<div style={{ color: 'red', marginLeft: '20%', fontSize: '1.5rem' }}><FaExclamation />{errors.email}</div>) : null}
+              </div>
+              <div className={classes.inputWraper}>
+                <label htmlFor="message">
+                  <MdMessage />
+                  {' '}
+                  Message
               </label> <br />
-                  <Field
-                    as="textarea"
-                    rows="4"
-                    placeholder="Enter your message"
-                    name="message"
-                    type="text"
-                    className={classes.inputMessage}
-                  />
-                  {errors.message && touched.message ? (<div style={{color:'red', marginLeft:'20%', fontSize:'1.5rem'}}><FaExclamation />{errors.message}</div>) : null}
-                </div>
-                <div className={classes.butWrapper}>
-                  <button
-                    className={classes.butSubmit}
-                    type="submit"
-                    disabled={isSubmitting}
-                  >
-                    Submit
+                <Field
+                  as="textarea"
+                  rows="4"
+                  placeholder="Enter your message"
+                  name="message"
+                  type="text"
+                  className={classes.inputMessage}
+                  className='inputMessage'
+                />
+                {errors.message && touched.message ? (<div style={{ color: 'red', marginLeft: '20%', fontSize: '1.5rem' }}><FaExclamation />{errors.message}</div>) : null}
+              </div>
+              <div className={classes.butWrapper}>
+                <button
+                  className={classes.butSubmit}
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  Submit
                   </button>
-                </div>
-              </Form>
-            )}
-          </Formik>
-          <Dialog
-            open={open}
-            TransitionComponent={Transition}
-            keepMounted
-            onClose={handleClose}
-            aria-labelledby="alert-dialog-slide-title"
-            aria-describedby="alert-dialog-slide-description"
-          >
-            <Container>
-              <DialogTitle disableTypography id="alert-dialog-slide-title">
-                <h5>Form Submitted!</h5>
-              </DialogTitle>
-              <ThankYou viewBox="0 0 1035 627" width={500} />
-              <DialogContent>
-                <Typography align="center">Thank you for contacting me!</Typography>
-              </DialogContent>
-              <DialogActions>
-                <Button autoFocus onClick={handleClose} color="primary">
-                  Close
+              </div>
+            </Form>
+          )}
+        </Formik>
+        <Dialog
+          open={open}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-slide-title"
+          aria-describedby="alert-dialog-slide-description"
+        >
+          <Container>
+            <DialogTitle disableTypography id="alert-dialog-slide-title">
+              <h5>Form Submitted!</h5>
+            </DialogTitle>
+            <ThankYou viewBox="0 0 1035 627" width={500} />
+            <DialogContent>
+              <Typography align="center">Thank you for contacting me!</Typography>
+            </DialogContent>
+            <DialogActions>
+              <Button autoFocus onClick={handleClose} color="primary">
+                Close
             </Button>
-              </DialogActions>
-            </Container>
-          </Dialog>
+            </DialogActions>
+          </Container>
+        </Dialog>
+      </div>
+      <div className='contactInfo'>
+        <div >
+          <MdEmail className="icon" />
+          <p>tzenebe3@gmail.com</p>
         </div>
-        <div className={classes.contactInfo}>
-          <div >
-            <MdEmail className="icon" />
-            <p>tzenebe3@gmail.com</p>
-          </div>
-          <div >
-            <MdPhone className="icon" />
-            <p>+1 202-569-4706</p>
-          </div>
-          <h3>Follow Me</h3>
-          <div className="contactIcons">
-            <a href="https://github.com/TeddyZenebe" target="_blank" ><FaGithub   /></a>{' '}
-            <FaLinkedin />{' '}
-            <FaFacebook />
-          </div>
+        <div >
+          <MdPhone className="icon" />
+          <p>+1 202-569-4706</p>
         </div>
+        <h3>Follow Me</h3>
+        <div className="contactIcons">
+          <a href="https://github.com/TeddyZenebe" target="_blank" ><FaGithub /></a>{' '}
+          <FaLinkedin />{' '}
+          <FaFacebook />
+        </div>
+      </div>
     </div>
   );
 }
